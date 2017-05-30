7b4a236238101a0f527b1c134e4aac1d80c322c9
amoliavko <ittgml@gmail.com>
2017-05-30 12:04:46 +0300

    Successful upload, parsing and download

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,8 @@
 from flask import Flask
 from flask_wtf.csrf import CsrfProtect
 
-UPLOAD_FOLDER = '/media/a_m/Wdisk/Python/Projects/Bank_parser/app/uploaded_file'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'csv', 'xls', 'xlsx'])
-
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-#app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
 app.config.from_object('config')
 CsrfProtect(app)
 
--- app/views.py
+++ app/views.py
@@ -5,34 +5,35 @@ from pandas import read_csv, read_excel
 import os
 from werkzeug.utils import secure_filename
 
-
 MAX_FILE_SIZE = 1024 * 1024 * 50 + 1
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'csv', 'xls', 'xlsx'])
 
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    args = {"method": "GET"}
+    args = {'method': 'GET'}
     form = LoadFile()
-    if form.is_submitted():
-        if request.method == "POST":
-            file = request.files["file"]
+    if form.is_submitted() and request.method == 'POST':
+            file = request.files['file']
             if bool(file.filename):
                 bank_select = form.bank_select.data
-                args["method"] = "POST"
+                args['method'] = 'POST'
 
-                if bank_select =="Avangard":
-                    lf1 = read_csv(file, encoding='cp1251')
+                if bank_select =='Avangard':
                     filename = secure_filename(file.filename)
-                    file_address = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                    file.save(file_address)
-                    return send_from_directory(file_address, filename)
-  #                  return redirect(url_for('index', filename=filename))
-  #                  return render_template("load_successful.html", bank_select=bank_select, file=link)
+                    file_address = os.path.join('static', filename)
+  #                  file.save(file_address)
+                    lf1 = read_csv(file, encoding='cp1251')
+                    out_file = open("{}/{}".format('static', filename), 'w')
+                    out_file.write(str(lf1))
+                    return render_template("load_successful.html", bank_select=bank_select, file=file_address)
+#                  return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+#                  return redirect(url_for('index', filename=filename))
                 else:
                     pass
     return render_template("index.html", args=args, form=form)
