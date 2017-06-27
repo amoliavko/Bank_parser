e391062c52ef0723edd23f6e33b5a1590dca5f11
amoliavko <ittgml@gmail.com>
2017-06-27 16:57:41 +0300

    Successful upload, parsing and download

--- app/views.py
+++ app/views.py
@@ -23,138 +23,49 @@ def index():
             if bool(file.filename):
                 bank_select = form.bank_select.data
                 args['method'] = 'POST'
+                filename = secure_filename(file.filename)
+                file_address = os.path.join('static', filename)
+                file.save(file_address)
 
                 if bank_select =='Bsbbank':
-                    filename = secure_filename(file.filename)
-                    file_address = os.path.join('static', filename)
-                    file.save(file_address)
                     out = BcbParser.file_parser(file_address)
 
-                    df = pd.DataFrame(out)
-                    df = df.rename(columns={0:'Банк',	1:'Дата', 2:'Дебет', 3:'Кредит', 4:'Валюта', 5:'Назначение', 6:'Агент', 7:'Счет', 8:'ЄДРПОУ'})
-                    writer =pd.ExcelWriter('static/result.xlsx', engine='xlsxwriter')
-                    df.to_excel(writer, sheet_name='Sheet1', index=False)
-                    writer.save()
-                    link = os.path.join('static', 'result.xlsx')
-
-                    return render_template("load_successful.html", file=out, link=link)
-
-                if bank_select =='Aval':
-                    filename = secure_filename(file.filename)
-                    file_address = os.path.join('static', filename)
-                    file.save(file_address)
+                elif bank_select =='Aval':
                     out = AvalParser.file_parser(file_address)
 
-                    df = pd.DataFrame(out)
-                    writer =pd.ExcelWriter('static/result.xlsx', engine='xlsxwriter')
-                    df.to_excel(writer, sheet_name='Sheet1', index=False)
-                    writer.save()
-                    link = os.path.join('static', 'result.xlsx')
-
-                    return render_template("load_successful.html", file=out, link=link)
-
-                if bank_select =='Centercredit':
-                    filename = secure_filename(file.filename)
-                    file_address = os.path.join('static', filename)
-                    file.save(file_address)
+                elif bank_select =='Centercredit':
                     out = Centercredit.file_parser(file_address)
 
-                    df = pd.DataFrame(out)
-                    writer =pd.ExcelWriter('static/result.xlsx', engine='xlsxwriter')
-                    df.to_excel(writer, sheet_name='Sheet1', index=False)
-                    writer.save()
-                    link = os.path.join('static', 'result.xlsx')
-
-                    return render_template("load_successful.html", file=out, link=link)
-
-
-                if bank_select =='Privat':
-                    filename = secure_filename(file.filename)
-                    file_address = os.path.join('static', filename)
-                    file.save(file_address)
+                elif bank_select =='Privat':
                     out = PrivatParser.file_parser(file_address)
 
-                    df = pd.DataFrame(out)
-                    writer =pd.ExcelWriter('static/result.xlsx', engine='xlsxwriter')
-                    df.to_excel(writer, sheet_name='Sheet1', index=False)
-                    writer.save()
-                    link = os.path.join('static', 'result.xlsx')
-
-                    return render_template("load_successful.html", file=out, link=link)
-
-
-                if bank_select =='Procreditbank':
-                    filename = secure_filename(file.filename)
-                    file_address = os.path.join('static', filename)
-                    file.save(file_address)
+                elif bank_select =='Procreditbank':
                     out = ProcreditbankParser.file_parser(file_address)
 
-                    df = pd.DataFrame(out)
-                    writer =pd.ExcelWriter('static/result.xlsx', engine='xlsxwriter')
-                    df.to_excel(writer, sheet_name='Sheet1', index=False)
-                    writer.save()
-                    link = os.path.join('static', 'result.xlsx')
-
-                    return render_template("load_successful.html", file=out, link=link)
-
-                if bank_select =='Vtb':
-                    filename = secure_filename(file.filename)
-                    file_address = os.path.join('static', filename)
-                    file.save(file_address)
+                elif bank_select =='Vtb':
                     out = VtbParser.file_parser(file_address)
 
-                    df = pd.DataFrame(out)
-                    writer =pd.ExcelWriter('static/result.xlsx', engine='xlsxwriter')
-                    df.to_excel(writer, sheet_name='Sheet1', index=False)
-                    writer.save()
-                    link = os.path.join('static', 'result.xlsx')
-
-                    return render_template("load_successful.html", file=out, link=link)
-
-
-                if bank_select =='BsbbankForeign':
-                    filename = secure_filename(file.filename)
-                    file_address = os.path.join('static', filename)
-                    file.save(file_address)
+                elif bank_select =='BsbbankForeign':
                     out = BcbForeignParser.file_parser(file_address)
 
-                    df = pd.DataFrame(out)
-                    writer =pd.ExcelWriter('static/result.xlsx', engine='xlsxwriter')
-                    df.to_excel(writer, sheet_name='Sheet1', index=False)
-                    writer.save()
-                    link = os.path.join('static', 'result.xlsx')
-
-                    return render_template("load_successful.html", file=out, link=link)
-
-                if bank_select =='Alfa':
-                    filename = secure_filename(file.filename)
-                    file_address = os.path.join('static', filename)
-                    file.save(file_address)
+                elif bank_select =='Alfa':
                     out = BP.file_parser(file_address)
 
-                    df = pd.DataFrame(out)
-                    writer =pd.ExcelWriter('static/result.xlsx', engine='xlsxwriter')
-                    df.to_excel(writer, sheet_name='Sheet1', index=False)
-                    writer.save()
-                    link = os.path.join('static', 'result.xlsx')
-
-                    return render_template("load_successful.html", file=out, link=link)
-
-
-                if bank_select =='UkrExim':
-                    filename = secure_filename(file.filename)
-                    file_address = os.path.join('static', filename)
-                    file.save(file_address)
+                elif bank_select =='UkrExim':
                     out = UkrEximParser.file_parser(file_address)
 
-                    df = pd.DataFrame(out)
-                    writer =pd.ExcelWriter('static/result.xlsx', engine='xlsxwriter')
-                    df.to_excel(writer, sheet_name='Sheet1', index=False)
-                    writer.save()
-                    link = os.path.join('static', 'result.xlsx')
-
-                    return render_template("load_successful.html", file=out, link=link)
-
                 else:
-                    pass
+                    return render_template("index.html", args=args, form=form)
+
+                df = pd.DataFrame(out)
+                df = df.rename(
+                    columns={0: 'Банк', 1: 'Дата', 2: 'Дебет', 3: 'Кредит', 4: 'Валюта', 5: 'Назначение', 6: 'Агент',
+                             7: 'Счет', 8: 'ЄДРПОУ'})
+                writer =pd.ExcelWriter('static/result.xlsx', engine='xlsxwriter')
+                df.to_excel(writer, sheet_name='Sheet1', index=False)
+                writer.save()
+                link = os.path.join('static', 'result.xlsx')
+
+                return render_template("load_successful.html", file=out, link=link)
+
     return render_template("index.html", args=args, form=form)
